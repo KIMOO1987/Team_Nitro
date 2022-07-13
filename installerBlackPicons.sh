eda1183fe0254edeaedf12d9050a0528cf084a11
kaleem87 <58071152+kaleem87@users.noreply.github.com>
2022-07-13 12:30:54 +0500

    Add files via upload

---
+++ installerBlackPicons.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+##setup command=wget http://tunisia-dreambox.info/TSplugins/NitroAdvanceFHD/picons/installerBlackPicons.sh -O - | /bin/sh
+## This script to downlaod and install Black Picon by () to inside plugin
+
+if [ ! -d /usr/lib64 ]; then
+	PLUGINPATH=/usr/lib/enigma2/python/Plugins/Extensions/NitroAdvanceFHD
+else
+	PLUGINPATH=/usr/lib64/enigma2/python/Plugins/Extensions/NitroAdvanceFHD
+fi
+
+## Remove old file from tmp directory
+[ -r /tmp/BlackPicons.tar.gz ] && rm -f /tmp/BlackPicons.tar.gz
+
+### Remove Currenty file from plugin
+rm -rf $PLUGINPATH/PICONS/emu
+rm -rf $PLUGINPATH/PICONS/piconSat
+rm -rf $PLUGINPATH/PICONS/piconProv
+rm -rf $PLUGINPATH/PICONS/piconCrypt
+
+# Download and install Black Picon
+cd /tmp
+set -e
+wget "http://tunisia-dreambox.info/TSplugins/RaedQuickSignal/picons/BlackPicons.tar.gz"
+tar -xzf BlackPicons.tar.gz -C /
+set +e
+cd ..
+
+### delete tmp files
+rm -f /tmp/BlackPicons.tar.gz
+sync
+echo ""
+echo ""
+echo "#########################################################"
+echo "#  NitroAdvanceFHD BLACK_PICONS INSTALLED SUCCESSFULLY  #"
+echo "#                     (Team Nitro)                      #"              
+echo "#########################################################"
+echo ""
+echo ""
+echo "#########################################################"
+echo "#            Press ok to Exit from Console              #"
+echo "#########################################################"
+exit 0
