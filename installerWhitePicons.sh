7ad26547dd44f590ab28f41a4a7cf3c0b82e6b09
kaleem87 <58071152+kaleem87@users.noreply.github.com>
2022-07-13 12:50:16 +0500

    Update installerWhitePicons.sh

--- installerWhitePicons.sh
+++ installerWhitePicons.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-##setup command=wget http://tunisia-dreambox.info/TSplugins/RaedQuickSignal/picons/installerWhitePicons.sh -O - | /bin/sh
-## This script to downlaod and install Black Picon by () to inside plugin
+##setup command=wget https://github.com/kaleem87/Team_Nitro/raw/main/installerWhitePicons.sh -O - | /bin/sh
+## This script to downlaod and install White Picons by () to inside plugin
 
 if [ ! -d /usr/lib64 ]; then
-	PLUGINPATH=/usr/lib/enigma2/python/Plugins/Extensions/RaedQuickSignal
+	PLUGINPATH=/usr/lib/enigma2/python/Plugins/Extensions/NitroAdvanceFHD
 else
-	PLUGINPATH=/usr/lib64/enigma2/python/Plugins/Extensions/RaedQuickSignal
+	PLUGINPATH=/usr/lib64/enigma2/python/Plugins/Extensions/NitroAdvanceFHD
 fi
 
 ## Remove old file from tmp directory
@@ -17,10 +17,10 @@ rm -rf $PLUGINPATH/PICONS/piconSat
 rm -rf $PLUGINPATH/PICONS/piconProv
 rm -rf $PLUGINPATH/PICONS/piconCrypt
 
-# Download and install White Picon
+# Download and install White Picons
 cd /tmp
 set -e
-wget "http://tunisia-dreambox.info/TSplugins/RaedQuickSignal/picons/WhitePicons.tar.gz"
+wget "https://github.com/kaleem87/Team_Nitro/raw/main/WhitePicons.tar.gz"
 tar -xzf WhitePicons.tar.gz -C /
 set +e
 cd ..
@@ -31,10 +31,8 @@ sync
 echo ""
 echo ""
 echo "#########################################################"
-echo "#       RaedQuickSignal INSTALLED SUCCESSFULLY          #"
-echo "#                 Raed  (fairbird)                      #"              
-echo "#                     support                           #"
-echo "#  https://www.tunisia-sat.com/forums/threads/2890989   #"
+echo "#  NitroAdvanceFHD WHITE_PICONS INSTALLED SUCCESSFULLY  #"
+echo "#                     (Team Nitro)                      #"              
 echo "#########################################################"
 echo ""
 echo ""
