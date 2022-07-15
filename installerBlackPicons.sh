806698ab267f1989e4218b04d8a35c8fcad046d3
kaleem87 <58071152+kaleem87@users.noreply.github.com>
2022-07-16 01:58:55 +0500

    Update installerBlackPicons.sh

--- installerBlackPicons.sh
+++ installerBlackPicons.sh
@@ -1,27 +1,12 @@
 #!/bin/sh
-##setup command=wget https://github.com/kaleem87/Team_Nitro/raw/main/installerBlackPicons.sh -O - | /bin/sh
-## This script to downlaod and install Black Picon by () to inside plugin
+##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/biko-73/NitroAdvanceFHD/main/installerBlackPicons.sh -O - | /bin/sh
+## This script to downlaod and install Black Picon by () to inside skin
 
-if [ ! -d /usr/lib64 ]; then
-	PLUGINPATH=/usr/lib/enigma2/python/Plugins/Extensions/NitroAdvanceFHD
-else
-	PLUGINPATH=/usr/lib64/enigma2/python/Plugins/Extensions/NitroAdvanceFHD
-fi
-
-## Remove old file from tmp directory
-[ -r /tmp/BlackPicons.tar.gz ] && rm -f /tmp/BlackPicons.tar.gz
-
-### Remove Currenty file from plugin
-rm -rf $PLUGINPATH/PICONS/emu
-rm -rf $PLUGINPATH/PICONS/piconSat
-rm -rf $PLUGINPATH/PICONS/piconProv
-rm -rf $PLUGINPATH/PICONS/piconCrypt
-
-# Download and install Black Picon
+# Download and install Black Picons
 cd /tmp
 set -e
-wget "https://drive.google.com/uc?export=download&id=1bpDnxvIPK5MEy4yQ_RGHrKyFPo8WoE4z"
-tar -xzf BlackPicons.tar.gz -C /
+wget "https://github.com/biko-73/NitroAdvanceFHD//raw/main/BlackPicons.tar.gz"
+tar xzvpf /tmp/BlackPicons.tar.gz  -C / 
 set +e
 cd ..
 
@@ -31,12 +16,6 @@ sync
 echo ""
 echo ""
 echo "#########################################################"
-echo "#  NitroAdvanceFHD BLACK_PICONS INSTALLED SUCCESSFULLY  #"
-echo "#                     (Team Nitro)                      #"              
-echo "#########################################################"
-echo ""
-echo ""
-echo "#########################################################"
-echo "#            Press ok to Exit from Console              #"
+echo "#               picon is downloaded                     #"
 echo "#########################################################"
 exit 0
