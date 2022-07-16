c3a658c97922e0adb7e28157ee945cd93b8ff2ca
kaleem87 <58071152+kaleem87@users.noreply.github.com>
2022-07-16 18:29:33 +0500

    Create installerBlackPicons.sh

---
+++ installerBlackPicons.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/biko-73/NitroAdvanceFHD/main/installerBlackPicons.sh -O - | /bin/sh
+## This script to downlaod and install Black Picon by () to inside skin
+
+# Download and install Black Picons
+cd /tmp
+set -e
+wget "https://github.com/biko-73/NitroAdvanceFHD//raw/main/BlackPicons.tar.gz"
+set +e
+cd ..
+sync
+echo ""
+echo ""
+echo "#########################################################"
+echo "#               picon is downloaded                     #"
+echo "#########################################################"
