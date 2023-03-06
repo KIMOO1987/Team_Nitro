94a9a72ea15e5ec8a9007c930b5781e7be89dc42
kaleem87 <58071152+kaleem87@users.noreply.github.com>
2023-03-06 19:38:48 +0500

    Update DesertFHD.sh

--- script/DesertFHD.sh
+++ script/DesertFHD.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ##########################################################
-version = 2.0
-description = "DesertFHD V2.0\n\n 1- Fix some missing screens\n\n 2- Fix missing CAM icons\n\n 3- Fix missing weather icons\n\n 4- Fix some Renders and Converters\n\n 5- Fix slow menu issue for some device\n\n 6- Fix auto Update feature in Plugin\n\n 7- add changelog in plugin\n\n"
+version= 2.0
+description="DesertFHD V2.0\n\n 1- Fix some missing screens\n\n 2- Fix missing CAM icons\n\n 3- Fix missing weather icons\n\n 4- Fix some Renders and Converters\n\n 5- Fix slow menu issue for some device\n\n 6- Fix auto Update feature in Plugin\n\n 7- add changelog in plugin\n\n"
 #########################################################
 # ----------------------------------------------------------------------------------------------------------
