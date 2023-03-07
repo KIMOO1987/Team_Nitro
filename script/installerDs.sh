84547fd80496a841a6f6af678d52ea58dd40d12c
kaleem87 <58071152+kaleem87@users.noreply.github.com>
2023-03-07 21:21:27 +0500

    Update installerDs.sh

--- script/installerDs.sh
+++ script/installerDs.sh
@@ -4,25 +4,34 @@ version=1.1
 description="DesertFHD" !!!
 #########################################################
 
+#########################################################
+MY_FILE="enigma2-plugin-skins-desertfhd_v2.0_all.ipk"
+MY_TMP_FILE="/var/volatile/tmp/"$MY_FILE
+#########################################################
+
 MY_SEP='============================================================='
 echo $MY_SEP
-echo 'Downloading DesertFHD' ...'
+echo 'Downloading '$MY_FILE' ...'
 echo $MY_SEP
 echo ''
 
 wget -O /var/volatile/tmp/enigma2-plugin-skins-desertfhd_v2.0_all.ipk --no-check-certificate "https://onedrive.live.com/download?cid=89E618BEC5025E42&resid=89E618BEC5025E42%212004&authkey=AEOSbylrP_BLMN0"
 
-if [ -f /var/volatile/tmp/enigma2-plugin-skins-desertfhd_v2.0_all.ipk ]; then
+rm -rf "/usr/lib/enigma2/python/Plugins/Extensions/TeamNitro"
+rm -rf "/usr/share/enigma2/DesertFHD"
+
+
+if [ -f $MY_TMP_FILE ]; then
 
 	echo ''
 	echo $MY_SEP
 	echo 'Extracting ...'
 	echo $MY_SEP
 	echo ''
-	opkg install /var/volatile/tmp/enigma2-plugin-skins-desertfhd_v2.0_all.ipk
+	opkg install $MY_TMP_FILE
 	MY_RESULT=$?
 
-	rm -f /var/volatile/tmp/enigma2-plugin-skins-desertfhd_v2.0_all.ipk > /dev/null 2>&1
+	rm -f $MY_TMP_FILE > /dev/null 2>&1
 
 	echo ''
 	if [ $MY_RESULT -eq 0 ]; then
