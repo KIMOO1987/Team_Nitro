f98ce14aec782a266406066e446f45ed99110fc9
kaleem87 <58071152+kaleem87@users.noreply.github.com>
2023-03-07 21:11:54 +0500

    Update installerDs.sh

--- script/installerDs.sh
+++ script/installerDs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 ##########################################################
-version=2.0
+version=1.1
 description="DesertFHD" !!!
 #########################################################
 
 #########################################################
-MY_FILE="enigma2-plugin-skins-desertfhd-v2.0-all.ipk"
+MY_FILE="enigma2-plugin-skins-desertfhd_v2.0_all.ipk"
 MY_TMP_FILE="/var/volatile/tmp/"$MY_FILE
 #########################################################
 
@@ -15,7 +15,7 @@ echo 'Downloading '$MY_FILE' ...'
 echo $MY_SEP
 echo ''
 
-wget -O /var/volatile/tmp/enigma2-plugin-skins-desertfhd-v2.0-all.ipk --no-check-certificate "https://onedrive.live.com/download?cid=89E618BEC5025E42&resid=89E618BEC5025E42%212004&authkey=AEOSbylrP_BLMN0"
+wget -O /var/volatile/tmp/enigma2-plugin-skins-desertfhd_v2.0_all.ipk --no-check-certificate "https://onedrive.live.com/download?cid=89E618BEC5025E42&resid=89E618BEC5025E42%212004&authkey=AEOSbylrP_BLMN0"
 
 rm -rf "/usr/lib/enigma2/python/Plugins/Extensions/TeamNitro"
 rm -rf "/usr/share/enigma2/DesertFHD"
@@ -25,10 +25,10 @@ if [ -f $MY_TMP_FILE ]; then
 
 	echo ''
 	echo $MY_SEP
-	echo 'Installation started'
+	echo 'Extracting ...'
 	echo $MY_SEP
 	echo ''
-	opkg install --force-reinstall $MY_TMP_FILE
+	opkg install $MY_TMP_FILE
 	MY_RESULT=$?
 
 	rm -f $MY_TMP_FILE > /dev/null 2>&1
@@ -36,7 +36,7 @@ if [ -f $MY_TMP_FILE ]; then
 	echo ''
 	if [ $MY_RESULT -eq 0 ]; then
          echo "########################################################################"
-         echo "#   TeamNitro Control Center & Skin DesertFHD INSTALLED SUCCESSFULLY   #"
+         echo "#		Skin DesertFHD INSTALLED SUCCESSFULLY                 #"
          echo "#                      BY TeamNitro - support on                       #"
          echo "#              https://www.tunisia-sat.com/forums/forums               #"
          echo "########################################################################"
